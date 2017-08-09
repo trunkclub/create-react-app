885f19cb4b31f4f37a5e7ff259a79255cb9c88df
Michael LaCroix <michael@lacroixdesign.net>
2017-08-09 15:52:58 -0500

    Switch back to hardcoded NODE_ENV, use TC_ENV for loading env files (#93)

--- packages/react-scripts/config/env/getTCEnv.js
+++ packages/react-scripts/config/env/getTCEnv.js
@@ -9,7 +9,7 @@ var env = process.env.NODE_ENV;
 // CRA assumes two environments: development or production.
 // There are some scenarios where we need to explicitly know that
 // we are in a staging environment.
-process.env.TC_ENV = env;
+process.env.TC_ENV = process.env.TC_ENV || env;
 process.env.TC_CLIENT_APP_NAME = pkg.name;
 process.env.TC_CLIENT_BUILD_COMMIT = git.long();
 process.env.TC_CLIENT_BUILD_TIME = (new Date()).toISOString();
